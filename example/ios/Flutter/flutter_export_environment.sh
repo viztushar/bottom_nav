77a25527cb9b97870280c7afbfba57e3ae93ecc6
Tushar <viztushar@gmail.com>
2021-09-30 22:32:41 +0530

    fix

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,9 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/Shared/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/Shared/Projects/flutter/plugin/bottom_nav/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
